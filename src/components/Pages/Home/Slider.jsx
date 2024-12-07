ef9c4f986325138a5f31971fcccc575ac073be8b
adnanahid <sikuahamed@gmail.com>
2024-12-07 22:50:56 +0600

    theme controler edited

--- src/components/Pages/Home/Slider.jsx
+++ src/components/Pages/Home/Slider.jsx
@@ -7,9 +7,14 @@ const Slider = () => {
   const AutoplaySlider = withAutoplay(AwesomeSlider);
 
   return (
-    <AutoplaySlider play={true} cancelOnInteraction={false} interval={3000}>
+    <AutoplaySlider
+      className="h-[1000px]"
+      play={true}
+      cancelOnInteraction={false}
+      interval={3000}
+    >
       <div className="w-full min-h-screen slider1">
-        <div className="min-h-screen w-full"></div>
+        <div></div>
       </div>
       <div className="w-full min-h-screen slider2">
         <div className="min-h-screen w-full"></div>
